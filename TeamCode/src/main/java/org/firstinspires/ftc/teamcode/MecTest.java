8a7d76c1c3399423465881a85ce59b4b8eebf7f5
snxxwy <mletendre3@icloud.com>
2025-10-23 12:24:29 -0700

    added basic mecanum teleop script

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecTest.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
-public class MecanumTeleOp extends LinearOpMode {
+public class MecTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         // Declare our motors
